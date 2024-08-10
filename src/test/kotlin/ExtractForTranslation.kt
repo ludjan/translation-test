d9682528528f7a496e0477f473c6e0ccf3721cce
ludjan <ludviggjansson@gmail.com>
2024-08-11 00:06:22 +0200

    Delete ExtractForTranslation.kt

--- src/test/kotlin/ExtractForTranslation.kt
+++
@@ -1,8 +0,0 @@
-@Target(AnnotationTarget.PROPERTY, AnnotationTarget.LOCAL_VARIABLE)
-@Retention(AnnotationRetention.SOURCE)
-annotation class ExtractForTranslation(
-    val context: String = "",
-    val key: String = "",
-    val description: String = "",
-    val sourceLanguage: String = "en"
-)
